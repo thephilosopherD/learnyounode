b6386a06cc458458246be2a7a155531b59970e07
daniel <daniel.pollman@gmail.com>
2015-01-20 20:00:54 -0800

    updated files

---
+++ filteredls.js
@@ -0,0 +1,28 @@
+var fs = require('fs')
+var pa = require('path')
+var ext ='.'+ process.argv[3]
+
+var filenames = fs.readdir(process.argv[2], 
+function callback (err, list){
+if (err) throw err;
+filenames = list;
+var filtered = filenames.filter(function (a){
+return (pa.extname(a) === ext);
+ });
+for (var i = 0; i < filtered.length; i++){
+console.log(filtered[i]);
+ }
+});
+
+//Official Solution: 
+
+//   var fs = require('fs')
+//    var path = require('path')
+    
+//    fs.readdir(process.argv[2], function (err, list) {
+//      list.forEach(function (file) {
+//        if (path.extname(file) === '.' + process.argv[3])
+//          console.log(file)
+//      })
+//    })
+
--- firstioa.js
+++ firstioa.js
@@ -3,7 +3,7 @@ var fs = require('fs')
 fs.readFile(process.argv[2], 'utf8', 
 function callback (err, line){
 if (err) throw err;
-console.log(line.split('\n').length-1);
+console.log(line.split('\n').length -1);
 });
 
 
