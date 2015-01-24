5214d6ed2181ed9626db1d0ec58d11c93fb91cd5
daniel <daniel.pollman@gmail.com>
2015-01-23 22:29:57 -0800

    file changes

--- modular.js
+++ modular.js
@@ -1,2 +1,3 @@
 var module = require('./module.js')
 
+module.read(process.argv[2], process.argv[3])
--- module.js
+++ module.js
@@ -1,11 +1,16 @@
 var fs = require('fs')
 var path = require('path')
 
-module.exports = function read (process.argv[2], process.argv[3], callback){
+module.exports = function read (file, ext, callback {
  error(function (err, data){
   if (err)
    return callback(err)
-
+  data = fs.readdir(file, function (err, list) {
+	  list.forEach(function (file) {
+		  if (path.extname(file) === ext)
+			  console.log(file)
+	  })
+  })
   callback(null, data)
  })
-}
+})
