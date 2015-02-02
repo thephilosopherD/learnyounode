29039e7aeb0ebcb55b289c7bc137d1d24a4db7d8
daniel <daniel.pollman@gmail.com>
2015-02-01 22:50:37 -0800

    complete

--- httpclient.js
+++ httpclient.js
@@ -1,4 +1,16 @@
 var http = require('http')
 
 http.get(process.argv[2], function callback (response) {
-	response.on('data', function (
+	response.setEncoding('utf8')
+	response.on('data', function (data) {
+			console.log(data)
+	})
+	response.on('error', function (e) {
+		console.log('Got an error here.')
+	})
+})
+
+//Official
+
+//  var http = require('http')                                                                      
+//    http.get(process.argv[2], function (response) {                                             //    response.setEncoding('utf8')                                                              	// 	  response.on('data', console.log)                                                            //    response.on('error', console.error)                                                         //   })    
