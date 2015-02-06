d97979d866a08815e06d661663b4f016cde02213
Dan <daniel.pollman@gmail.com>
2015-02-05 23:16:30 -0800

    complete

--- babysteps.js
+++ babysteps.js
@@ -1,10 +1,10 @@
-var arraylength = process.argv.length;
-var sum = 0;
+var arraylength = process.argv.length
+var sum = 0
 
 for (i = 2; i < arraylength; i++)
 {
-sum += +process.argv[i]; //Also, Number(process.argv[i]); Either works. 
+sum += +process.argv[i] //Also, Number(process.argv[i]); Either works.  
 }
 
-console.log(sum);
+console.log(sum)
 
