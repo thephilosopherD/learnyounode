38cbea04e2586e6904702441c076a1a51317402c
Dan <daniel.pollman@gmail.com>
2015-03-25 21:03:18 -0700

    complete

--- filteredls.js
+++ filteredls.js
@@ -17,7 +17,7 @@ console.log(filtered[i]);
 
 //   var fs = require('fs')
 //    var path = require('path')
-    
+
 //    fs.readdir(process.argv[2], function (err, list) {
 //      list.forEach(function (file) {
 //        if (path.extname(file) === '.' + process.argv[3])
