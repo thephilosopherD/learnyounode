8a190b36214b6daef1c8055b3748746cb36ab6f8
Dan <daniel.pollman@gmail.com>
2015-03-31 23:06:05 -0700

    complete

--- firstioa.js
+++ firstioa.js
@@ -11,7 +11,7 @@ console.log(line.split('\n').length -1);
 //    var file = process.argv[2]
     
 //    fs.readFile(file, function (err, contents) {
-      // fs.readFile(file, 'utf8', callback) can also be used
+//    //fs.readFile(file, 'utf8', callback) can also be used
 //      var lines = contents.toString().split('\n').length - 1
 //      console.log(lines)
 //    })
